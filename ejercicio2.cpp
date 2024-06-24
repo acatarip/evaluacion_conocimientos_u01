3e13a1022b68ee9511bbd856e1f89028b3f77792
acatarip <acatarip@unjbg.edu.pe>
2024-06-23 22:48:48 -0500

    c9: Se agrego un while para determinar la cantidad de divisores comunes de a y b. Se agrego un if que escribe Son PESI o No son PESI.

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -2,10 +2,21 @@
 #include <iostream>
 using namespace std;
 int main (){
-	int a, b;
+	int a, b, i, c;
 	cout<<"Determinar si dos numeros son PESI"<<endl;
 	cout<<"Ingrese primer numero: ";cin>>a;
 	cout<<"Ingrese segundo numero: ";cin>>b;
-	
+	c=0;
+	i=1;
+	while ((i<=a) && (i<=b)){
+		if ((a%i==0) && (b%i==0)){
+			c=c+1;
+		}
+		i=i+1;
+	}
+	if (c>1)
+		cout<<"No son PESI.";
+	else
+		cout<<"Son PESI.";
 	return 0;
 }
