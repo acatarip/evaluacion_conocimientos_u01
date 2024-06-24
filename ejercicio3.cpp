a0834353153cfbd6869503ffbfe4504f1787c4a2
acatarip <acatarip@unjbg.edu.pe>
2024-06-23 23:45:14 -0500

    c12: Se agrego la funcion suma.

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -1,6 +1,9 @@
 //Ejercicio 3: calcular la suma s=i! /2i para i desde 1 hasta n
 #include <iostream>
 using namespace std;
+
+double suma (int a);
+
 int main (){
 	int n;
 	double sum;
@@ -9,4 +12,13 @@ int main (){
 	cout<<"La sumatoria es "<<n<<".";
 	
 	return 0;
+}
+
+double suma (int a){
+	double s, i;
+	s=0;
+	for (i=1; i<=a; i++){
+		s=s + (factorial(i) /2*i);
+	}
+	return s;
 }
