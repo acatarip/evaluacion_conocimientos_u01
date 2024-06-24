d7afce039c1395d4501c90eb940c7d3d810a9088
acatarip <acatarip@unjbg.edu.pe>
2024-06-23 22:32:50 -0500

    c8: creacion ejercicio2.cpp

---
+++ ejercicio2.cpp
@@ -0,0 +1,11 @@
+//Ejercicio 2: determinar si dos numeros son PESI
+#include <iostream>
+using namespace std;
+int main (){
+	int a, b;
+	cout<<"Determinar si dos numeros son PESI"<<endl;
+	cout<<"Ingrese primer numero: ";cin>>a;
+	cout<<"Ingrese segundo numero: ";cin>>b;
+	
+	return 0;
+}
