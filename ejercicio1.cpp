0b4f47ad8c8224d91ca60f67c93381336d216063
acatarip <acatarip@unjbg.edu.pe>
2024-06-23 22:05:07 -0500

    c4: añadir variables en ejercicio1.cpp

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -1,1 +1,13 @@
 //Ejercicio 1: diseñe una calculadora que pida dos numeros y un operador
+#include <iostream>
+using namespace std;
+int main (){
+	int op1, op2;
+	char op;
+	cout<<"Ingrese primer numero: "; cin >>op1;
+	cout<<"Ingrese segundo numero: "; cin >>op2;
+	cout<<"Ingrese operador: "; cin>>op;
+	
+	
+	return 0;
+}
