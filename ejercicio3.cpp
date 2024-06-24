2339cf7ce05700705167c1c8a1b399ffc49aa022
acatarip <acatarip@unjbg.edu.pe>
2024-06-24 00:14:29 -0500

    c13: Se agrego la funcion factorial.

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -3,22 +3,33 @@
 using namespace std;
 
 double suma (int a);
+double factorial (int b);
 
 int main (){
 	int n;
 	double sum;
 	cout<<"Ingrese n: "; cin>>n;
 	sum = suma(n);
-	cout<<"La sumatoria es "<<n<<".";
+	cout<<"La sumatoria es "<<sum<<".";
 	
 	return 0;
 }
 
 double suma (int a){
-	double s, i;
+	double s;
+	int i;
 	s=0;
 	for (i=1; i<=a; i++){
-		s=s + (factorial(i) /2*i);
+		s=s + factorial(i) /(2*i);
 	}
 	return s;
+}
+
+double factorial (int b){
+	double f, i;
+	f=1;
+	for (i=2; i<=b; i++){
+		f=f*i;
+	}
+	return f;
 }
