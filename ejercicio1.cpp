d30d7f73e0f64c15aaa89efc38d55fa5b504407b
acatarip <acatarip@unjbg.edu.pe>
2024-06-23 22:13:42 -0500

    c5: Se agrego switch. Se cambio tipo de variable de op1 y op2.

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -2,12 +2,25 @@
 #include <iostream>
 using namespace std;
 int main (){
-	int op1, op2;
+	float op1, op2, r;
 	char op;
 	cout<<"Ingrese primer numero: "; cin >>op1;
 	cout<<"Ingrese segundo numero: "; cin >>op2;
 	cout<<"Ingrese operador: "; cin>>op;
-	
-	
+	switch (op){
+		case '+':
+			r=op1+op2;
+		break;
+		case '-':
+			r=op1-op2;
+		break;
+		case '*':
+			r=op1*op2;
+		break;
+		case '/':
+			r=op1/op2;
+		break;
+	}
+	cout<<"El resultado es "<<r;
 	return 0;
 }
